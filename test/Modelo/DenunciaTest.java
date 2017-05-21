4e5b7649c857236eae46a834e93aba9b64f72bb1
Tatiana Dantas <tatiana_dantas@ymail.com>
2017-05-21 14:10:25 -0300

    teste por campos, com entradas setadas pelo plano de teste2

--- test/Modelo/DenunciaTest.java
+++ test/Modelo/DenunciaTest.java
@@ -36,24 +36,39 @@ public class DenunciaTest {
 
     @Before
     public void setUp() {
-        titulo = "titulo";
-        tituloMaior = "kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk";
-        tipo.add("Abandono");
-        tipo.add("Maus Tratos");
-        tipoErrado = "tipoErrado";
+        titulo = "titulo";                        //Título válido
+        tituloMaior = "kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk";  // Título inválido
+        tipo.add("Abandono");         // Tipos válidos
+        tipo.add("Maus Tratos");     // Tipos válidos
+        tipoErrado = "tipoErrado";  // Tipo inválido
         foto = "foto.png";
-        frase = "frase";
+        frase = "frase";                     // frase válida para descrição e localização
         fraseMaior = "The Inuit and Yupik people carved Inuit snow goggles from caribou antler, wood, and shell to help prevent snow blindness. "
                 + "The goggles were curved to fit the user's face and had a large groove cut in the back to allow for the nose. A long thin slit was cut through the "
                 + "goggles to allow in a small amount of light, diminishing subsequent ultraviolet rays. The goggles were held to the head by a cord made of caribou sinew."
                 + "In the early 20th century, goggles were worn by drivers of uncovered cars to prevent irritation of the eyes by dust or wind.[1] Likewise in the first ten years after t"
                 + "he invention of the airplane in 1903 goggles became a necessity as wind blow became more severe as aircraft speeds increased and as "
                 + "protection against bugstrikes at high altitudes. The first pilot to wear goggles was probably Charles Manly in his failed attempt to fly Samuel"
-                + " Langley's aerodrome in 1903.";
+                + " Langley's aerodrome in 1903.";     // frase inválida
 
-        d0 = new Denuncia(0, titulo, frase, foto, tipo.get(0), frase);
-        d = new Denuncia(1, null, frase, foto, tipo.get(0), frase);
-         d2 = new Denuncia(2, tituloMaior, frase, foto, tipo.get(0), frase);
+        d0 = new Denuncia(0, titulo, frase, foto, tipo.get(0), frase);                                     //Classe válida
+        // Teste Titulo
+        d1 = new Denuncia(1, null, frase, foto, tipo.get(0), frase);                                       //Classe inválida 1
+        d2 = new Denuncia(2, tituloMaior, frase, foto, tipo.get(0), frase);                          //Classe inválida 2
+       //Teste Descrição
+        d3 = new Denuncia(3, titulo, fraseMaior, foto, tipo.get(0), frase);                          //Classe inválida 3
+        d4 = new Denuncia(4, titulo, null, foto, tipo.get(0), frase);                                       //Classe inválida 4
+        //Teste Foto
+        d5 = new Denuncia(5, titulo, frase, foto, tipo.get(0), frase);                                   //Classe inválida 5
+        d6 = new Denuncia(6, titulo, frase, foto, tipo.get(0), frase);                                   //Classe inválida 6
+        d7 = new Denuncia(7, titulo, frase, foto, tipo.get(0), frase);                                   //Classe inválida 7
+        //Teste Tipo
+        d8 = new Denuncia(8, titulo, frase, foto, tipoErrado, frase);                                  //Classe inválida 8
+        //Teste Localição
+        d9 = new Denuncia(9, titulo, frase, foto, tipo.get(0), null);                                     //Classe inválida 9
+        d10 = new Denuncia(10, titulo, frase, foto, tipo.get(0), fraseMaior);                   //Classe inválida 10
+        
+        d = d10;       //Classe sendo testada
     }
 
     @After
@@ -66,15 +81,16 @@ public class DenunciaTest {
     @Test
     public void testTituloDenuncia() {
         System.out.println("TITULO");
-        if( d.getTituloDenuncia() != null) {
+        if (d.getTituloDenuncia() != null) {
             System.out.println("Titulo não é nulo");
-            if(!maior(d.getTituloDenuncia(), 30)){
-            System.out.println("Tamanho dentro do esperado: " + d.getTituloDenuncia().length() + "\n");    
-            }else{
-               System.out.println("Tamanho maior que o esperado: " + d.getTituloDenuncia().length() + "\n");    
+            if (!maior(d.getTituloDenuncia(), 30)) {
+                System.out.println("Tamanho dentro do esperado: " + d.getTituloDenuncia().length() + "\n");
+            } else {
+                System.out.println("Tamanho maior que o esperado: " + d.getTituloDenuncia().length() + "\n");
             }
-        }else
-              System.out.println("Campo nulo");    
+        } else {
+            System.out.println("Campo nulo");
+        }
     }
 
     /**
@@ -83,15 +99,16 @@ public class DenunciaTest {
     @Test
     public void testDescricaoDenuncia() {
         System.out.println("DESCRIÇÃO");
-         if( d.getDescricaoDenuncia()!= null) {
+        if (d.getDescricaoDenuncia() != null) {
             System.out.println("Descrição não é nulo");
-            if(!maior(d.getDescricaoDenuncia(), 255)){
-            System.out.println("Tamanho dentro do esperado: "+ d.getDescricaoDenuncia().length() + "\n");    
-            }else{
-               System.out.println("Tamanho maior que o esperado: " + d.getDescricaoDenuncia().length() + "\n");    
+            if (!maior(d.getDescricaoDenuncia(), 255)) {
+                System.out.println("Tamanho dentro do esperado: " + d.getDescricaoDenuncia().length() + "\n");
+            } else {
+                System.out.println("Tamanho maior que o esperado: " + d.getDescricaoDenuncia().length() + "\n");
             }
-        }else
-              System.out.println("Campo nulo");
+        } else {
+            System.out.println("Campo nulo");
+        }
     }
 
     /**
@@ -99,10 +116,11 @@ public class DenunciaTest {
      */
     @Test
     public void testTipoDenuncia() {
-        if( tipo.contains( d.getTipoDenuncia()))
+        if (tipo.contains(d.getTipoDenuncia())) {
             System.out.println("Campo válido");
-        else
-            System.out.println("Campo válido" + d.getTipoDenuncia()); 
+        } else {
+            System.out.println("Campo válido" + d.getTipoDenuncia());
+        }
     }
 
     /**
@@ -110,27 +128,17 @@ public class DenunciaTest {
      */
     @Test
     public void testLocalizacaoDenuncia() {
-          System.out.println("LOCALIZAÇÃO");
-         if( d.getDescricaoDenuncia()!= null) {
+        System.out.println("LOCALIZAÇÃO");
+        if (d.getDescricaoDenuncia() != null) {
             System.out.println("Descrição não é nulo");
-            if(!maior(d.getLocalizacao(), 255)){
-            System.out.println("Tamanho dentro do esperado: "+ d.getLocalizacao().length() + "\n");    
-            }else{
-               System.out.println("Tamanho maior que o esperado: " + d.getLocalizacao().length() + "\n");    
+            if (!maior(d.getLocalizacao(), 255)) {
+                System.out.println("Tamanho dentro do esperado: " + d.getLocalizacao().length() + "\n");
+            } else {
+                System.out.println("Tamanho maior que o esperado: " + d.getLocalizacao().length() + "\n");
             }
-        }else
-              System.out.println("Campo nulo");
-    }
-    
-    @Test
-    public void testNull(){
-        // Testando campos null para Título, Descrição e Localizaçao
-        assertNotNull(d1.getTituloDenuncia());
-        assertNotNull(d0.getTituloDenuncia());
-        assertNotNull(d1.getDescricaoDenuncia());
-        assertNotNull(d0.getDescricaoDenuncia());
-        assertNotNull(d1.getLocalizacao());
-        assertNotNull(d0.getLocalizacao());
+        } else {
+            System.out.println("Campo nulo");
+        }
     }
 
     public boolean maior(String s, int i) {
@@ -142,5 +150,5 @@ public class DenunciaTest {
         }
         return a;
     }
- 
+
 }
